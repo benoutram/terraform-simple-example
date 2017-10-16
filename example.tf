b8308a1d275c51874f2ff785ed5b793e21d9abdd
Ben Outram <ben@hiveit.co.uk>
2017-10-16 14:47:43 +0100

    Adding elastic ip address.

--- example.tf
+++ example.tf
@@ -8,3 +8,11 @@ resource "aws_instance" "example" {
   ami           = "${lookup(var.amis, var.region)}"
   instance_type = "t2.micro"
 }
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
+}
+
+output "ip" {
+  value = "${aws_eip.ip.public_ip}"
+}
